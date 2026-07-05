114d3642d8ac66dc7ba96c8b9eedb2607799e9d7
ehsan <mupalvan1@gmail.com>
2026-07-04 20:52:53 -0700

    "نصب و تنظیم Django REST Framework
    - نصب djangorestframework و simplejwt
    - تنظیمات احراز هویت JWT
    - تنظیمات پاگینیشن (صفحه‌بندی)
    - تنظیمات توکن‌های دسترسی"

--- config/settings/base.py
+++ config/settings/base.py
@@ -25,6 +25,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # کتابخانه‌های شخص ثالث
+    'rest_framework',
+    'rest_framework_simplejwt',
+    
+    # اپلیکیشن‌های خودمون
     'accounts',
     'products',
     'cart',
@@ -95,3 +101,24 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # مدل کاربر سفارشی
 AUTH_USER_MODEL = 'accounts.User'
+# تنظیمات Django REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
+
+# تنظیمات JWT
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+}
